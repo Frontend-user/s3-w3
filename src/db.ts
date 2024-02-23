80e9633a832e342571302ba303bd96f0a08df5e7
Robert <robersargsyan2023@gmail.com>
2024-02-23 20:42:53 +0300

    deploy

--- src/db.ts
+++ src/db.ts
@@ -5,62 +5,17 @@ import dotenv from 'dotenv'
 import {PostCreateType, PostEntityType} from "./common/types/post-type";
 import {UserCreateType, UserEmailEntityType, UserHashType} from "./users/types/user-types";
 import mongoose from "mongoose";
+import {
+    blogSchema,
+    commentSchema,
+    deviceSchema,
+    postSchema,
+    recoveryCodeSchema,
+    tokenSchema,
+    userSchema
+} from "./schemas/schemas";
 
-export const commentSchema = new mongoose.Schema({
-    content: {type: String, required: true},
-    commentatorInfo:{
-    userId: {type: String, required: true},
-    userLogin: {type: String, required: true}
-    },
-    createdAt: {type: String, required: true}
-})
-export const tokenSchema = new mongoose.Schema({
-    refreshToken: {type: String, required: true}
-})
 
-export const postSchema = new mongoose.Schema<PostEntityType>({
-    title: {type: String, required: true},
-    shortDescription: {type: String, required: true},
-    content: {type: String, required: true},
-    blogName: {type: String, required: true},
-    createdAt: {type: String, required: true},
-    blogId: {type: String || ObjectId, required: true}
-})
-export const blogSchema = new mongoose.Schema<BlogEntityType>({
-    name: {type: String, required: true},
-    description: {type: String, required: true},
-    websiteUrl: {type: String, required: true},
-    createdAt: {type: String, required: true},
-    isMembership: {type: Boolean, required: true},
-});
-export const recoveryCodeSchema = new mongoose.Schema<RecoveryCodeType>({
-    userId: {type: ObjectId, required: true},
-    recoveryCode: {type: String, required: true},
-    email: {type: String, required: true},
-})
-export const userSchema = new mongoose.Schema<UserEmailEntityType>({
-    accountData: {
-        login: {type: String, required: true},
-        email: {type: String, required: true},
-        createdAt: {type: String, required: true},
-    },
-    passwordSalt: {type: String, required: true},
-    passwordHash: {type: String, required: true},
-    emailConfirmation: {
-        confirmationCode: {type: String, required: true},
-        expirationDate: {type: String, required: true} || {type: Date, required: true}
-    },
-    isConfirmed: {type: Boolean, required: true},
-    isCreatedFromAdmin: {type: Boolean, required: true}
-});
-
-export const deviceSchema = new mongoose.Schema({
-    userId: {type: ObjectId, required: true},
-    ip: {type: String, required: true},
-    title: {type: String, required: true},
-    lastActiveDate: {type: String, required: true},
-    deviceId:   {type: String, required: true},
-});
 dotenv.config()
 const url = process.env.MONGO_URL
 
